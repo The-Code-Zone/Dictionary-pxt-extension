8be317c4293002fcac6b9aedcbabf07340ed5e51
DexDoogan <107511541+DexDoogan@users.noreply.github.com>
2023-11-21 16:05:10 +0000

    interface started

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
+interface Dictionary<K, V> {
 
+    
+    set(key: K, value: V): void;
+
+    get(key: K): V;
+
+    delete(key: K): void;
+
+    size(): number;
+
+    clear(): void;
+
+    containsKey(key: K): boolean;
+
+    containsValue(value: V): boolean;
+
+}
