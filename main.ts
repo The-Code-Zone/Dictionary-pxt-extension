41f27ca9f0f89837f506cb126052924f28984e50
DexDoogan <107511541+DexDoogan@users.noreply.github.com>
2023-11-21 16:24:41 +0000

    blocks attempt one set function

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
-interface Dictionary<K, V> {
+/// <reference no-default-lib="true"/>
 
+interface Dictionary<K, V> {
     
+    /**
+    * Append a new key, value pair to the dictionary.
+    */
+    //% help=dictionary/set
+    //% shim=Dictionary_::set weight=50
+    //% blockId="dictionary_set" block="keys %list| values %list| add key %value| add value %value| to dictionary" blockNamespace="dictionary"
+    //% group="Modify"
     set(key: K, value: V): void;
 
     get(key: K): V;
