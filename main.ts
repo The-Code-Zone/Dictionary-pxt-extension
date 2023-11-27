e150326a632e5863bb06c0678858a191d71f90f0
DexDoogan <107511541+DexDoogan@users.noreply.github.com>
2023-11-27 17:05:52 +0000

    change to class

--- main.ts
+++ main.ts
@@ -1,26 +1,103 @@
-/// <reference no-default-lib="true"/>
+class Dictionary<Keys, Values> {
+    private keys: Keys[]
+    private values: Values[]
 
-interface Dictionary<K, V> {
-    
-    /**
-    * Append a new key, value pair to the dictionary.
-    */
-    //% help=dictionary/set
-    //% shim=Dictionary_::set weight=50
-    //% blockId="dictionary_set" block="keys %list| values %list| add key %value| add value %value| to dictionary" blockNamespace="dictionary"
-    //% group="Modify"
-    set(key: K, value: V): void;
+    constructor(keys: Keys[], values: Values[]) {
+        if(this.keys.length === this.values.length){
+            this.keys = keys
+            this.values = values
+        }
+        else if(this.keys.length>this.values.length){
+            throw console.error("There are Keys without Values")
+        }
+        else{
+            throw console.error("There are Values without Keys")
+        }
+    }
 
-    get(key: K): V;
+    public set(key: Keys, value: Values) {
+        if (this.keys.indexOf(key) != -1) {
+            this.keys.push(key)
+            this.values.push(value)
+        }
+        else {
+            throw console.error("This key already exists. Keys must be unique.");
+        }
+    }
 
-    delete(key: K): void;
+    public get(): [Keys[], Values[]] {
+        return [this.keys, this.values]
+    }
 
-    size(): number;
+    public get_keys_list(): Keys[] {
+        return this.keys
+    }
 
-    clear(): void;
+    public get_values_list(): Values[] {
+        return this.values
+    }
 
-    containsKey(key: K): boolean;
+    public getValue(key: Keys): Values {
+        if (this.keys.indexOf(key) != -1) {
+            return this.values[this.keys.indexOf(key)]
+        }
+        else {
+            throw console.error("This key does not exist")
+        }
+    }
 
-    containsValue(value: V): boolean;
+    public getKey(value: Values): Keys[] {
+        if (this.values.indexOf(value) != -1) {
+            let answer: Keys[] = []
+            for (let i = 0; i < this.values.length; i++) {
+                if (value === this.values[i]) {
+                    answer.push(this.keys[i])
+                }
+            }
+            return answer
+        }
+        else {
+            throw console.error("This key does not exist")
+        }
+    }
+
+    public remove(key: Keys, value: Values) {
+        if (this.keys.indexOf(key) != -1) {
+            if (this.values[this.keys.indexOf(key)] === value) {
+                this.keys.splice(this.keys.indexOf(key), 1)
+                this.values.splice(this.values.indexOf(value), 1)
+            }
+        }
+        else {
+            throw console.error("That is not a valid key:value pair")
+        }
+    }
+
+    public remove_first() {
+        this.keys.shift()
+        this.values.shift()
+    }
+
+    public remove_last() {
+        this.keys.pop()
+        this.values.pop()
+    }
+
+    public toString(): string[] {
+        let answer: string[] = []
+        for (let i = 0; i < this.keys.length; i++) {
+            answer.push(this.keys[i] + ":" + this.values[i])
+        }
+        return answer
+    }
+
+    public entryToString(key: Keys, value: Values): string {
+        return key + ":" + value
+    }
+
+    public clean(){
+        this.keys = []
+        this.values = []
+    }
 
 }
