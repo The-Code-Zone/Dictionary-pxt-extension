3dff92748629e5c4c79bcac934d7f1141809b847
DexDoogan <107511541+DexDoogan@users.noreply.github.com>
2023-11-27 18:35:56 +0000

    namespace added

--- main.ts
+++ main.ts
@@ -1,103 +1,115 @@
-class Dictionary<Keys, Values> {
-    private keys: Keys[]
-    private values: Values[]
+//% color=#e6e6fa
+//% weight=79
+//% icon="\uf240"
+//% blockGap=8 block="Dictionary"
+//% groups='["Create"]'
+namespace Dictionary{
+    
+    export class Dictionary<Keys, Values> {
+        private keys: Keys[]
+        private values: Values[]
 
-    constructor(keys: Keys[], values: Values[]) {
-        if(this.keys.length === this.values.length){
-            this.keys = keys
-            this.values = values
+        constructor(keys: Keys[], values: Values[]) {
+            if(this.keys.length === this.values.length){
+                this.keys = keys
+                this.values = values
+            }
+            else if(this.keys.length>this.values.length){
+                throw console.error("There are Keys without Values")
+            }
+            else{
+                throw console.error("There are Values without Keys")
+            }
         }
-        else if(this.keys.length>this.values.length){
-            throw console.error("There are Keys without Values")
-        }
-        else{
-            throw console.error("There are Values without Keys")
-        }
-    }
 
-    public set(key: Keys, value: Values) {
-        if (this.keys.indexOf(key) != -1) {
-            this.keys.push(key)
-            this.values.push(value)
+        public set(key: Keys, value: Values) {
+            if (this.keys.indexOf(key) != -1) {
+                this.keys.push(key)
+                this.values.push(value)
+            }
+            else {
+                throw console.error("This key already exists. Keys must be unique.");
+            }
         }
-        else {
-            throw console.error("This key already exists. Keys must be unique.");
+
+        public get(): [Keys[], Values[]] {
+            return [this.keys, this.values]
         }
-    }
 
-    public get(): [Keys[], Values[]] {
-        return [this.keys, this.values]
-    }
-
-    public get_keys_list(): Keys[] {
-        return this.keys
-    }
-
-    public get_values_list(): Values[] {
-        return this.values
-    }
-
-    public getValue(key: Keys): Values {
-        if (this.keys.indexOf(key) != -1) {
-            return this.values[this.keys.indexOf(key)]
+        public get_keys_list(): Keys[] {
+            return this.keys
         }
-        else {
-            throw console.error("This key does not exist")
-        }
-    }
 
-    public getKey(value: Values): Keys[] {
-        if (this.values.indexOf(value) != -1) {
-            let answer: Keys[] = []
-            for (let i = 0; i < this.values.length; i++) {
-                if (value === this.values[i]) {
-                    answer.push(this.keys[i])
+        public get_values_list(): Values[] {
+            return this.values
+        }
+
+        public getValue(key: Keys): Values {
+            if (this.keys.indexOf(key) != -1) {
+                return this.values[this.keys.indexOf(key)]
+            }
+            else {
+                throw console.error("This key does not exist")
+            }
+        }
+
+        public getKey(value: Values): Keys[] {
+            if (this.values.indexOf(value) != -1) {
+                let answer: Keys[] = []
+                for (let i = 0; i < this.values.length; i++) {
+                    if (value === this.values[i]) {
+                        answer.push(this.keys[i])
+                    }
                 }
+                return answer
+            }
+            else {
+                throw console.error("This key does not exist")
+            }
+        }
+
+        public remove(key: Keys, value: Values) {
+            if (this.keys.indexOf(key) != -1) {
+                if (this.values[this.keys.indexOf(key)] === value) {
+                    this.keys.splice(this.keys.indexOf(key), 1)
+                    this.values.splice(this.values.indexOf(value), 1)
+                }
+            }
+            else {
+                throw console.error("That is not a valid key:value pair")
+            }
+        }
+
+        public remove_first() {
+            this.keys.shift()
+            this.values.shift()
+        }
+
+        public remove_last() {
+            this.keys.pop()
+            this.values.pop()
+        }
+
+        public toString(): string[] {
+            let answer: string[] = []
+            for (let i = 0; i < this.keys.length; i++) {
+                answer.push(this.keys[i] + ":" + this.values[i])
             }
             return answer
         }
-        else {
-            throw console.error("This key does not exist")
+
+        public entryToString(key: Keys, value: Values): string {
+            return key + ":" + value
+        }
+
+        public clean(){
+            this.keys = []
+            this.values = []
         }
     }
 
-    public remove(key: Keys, value: Values) {
-        if (this.keys.indexOf(key) != -1) {
-            if (this.values[this.keys.indexOf(key)] === value) {
-                this.keys.splice(this.keys.indexOf(key), 1)
-                this.values.splice(this.values.indexOf(value), 1)
-            }
-        }
-        else {
-            throw console.error("That is not a valid key:value pair")
-        }
+    export function create(keys: any[], values: any[]){
+        
     }
-
-    public remove_first() {
-        this.keys.shift()
-        this.values.shift()
-    }
-
-    public remove_last() {
-        this.keys.pop()
-        this.values.pop()
-    }
-
-    public toString(): string[] {
-        let answer: string[] = []
-        for (let i = 0; i < this.keys.length; i++) {
-            answer.push(this.keys[i] + ":" + this.values[i])
-        }
-        return answer
-    }
-
-    public entryToString(key: Keys, value: Values): string {
-        return key + ":" + value
-    }
-
-    public clean(){
-        this.keys = []
-        this.values = []
-    }
-
 }
+
