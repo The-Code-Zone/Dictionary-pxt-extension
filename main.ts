cc92d25515be8027c605bae767980d0dcb9c5949
DexDoogan <107511541+DexDoogan@users.noreply.github.com>
2023-12-13 17:28:48 +0000

    i think finished, sen to Tom for check

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-/**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
- */
 //% color=#e6e6fa
 //% weight=79
 //% icon="\uf240"
