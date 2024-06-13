25ec09e15854f7b6d25f21ccac4dfb652669da0a
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2024-06-13 17:43:56 +0100

    1.0.2

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"/simulator.html","cdnUrl":"/","version":"1.0.1","target":"arcade","targetVersion":"2.0.3","repo":"tomhausen/dictionary-extension-makecode"}
+// meta={"simUrl":"/simulator.html","cdnUrl":"/","version":"1.0.2","target":"arcade","targetVersion":"2.0.3","repo":"tomhausen/dictionary-extension-makecode"}
 // total=1248824 new=5.84% cached=88.49% other=5.67%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P71433(s) {
+function _main___P72619(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -831,13 +831,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P71433.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"test.ts","functionName":"<main>","argumentNames":[]}
-_main___P71433.continuations = [  ]
+_main___P72619.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"test.ts","functionName":"<main>","argumentNames":[]}
+_main___P72619.continuations = [  ]
 
-function _main___P71433_mk(s) {
+function _main___P72619_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P71433, depth: s.depth + 1,
+        parent: s, fn: _main___P72619, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42354,5 +42354,5 @@ const sprites_BaseSpriteSayRenderer__C55265_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P71433
+return _main___P72619
 })
